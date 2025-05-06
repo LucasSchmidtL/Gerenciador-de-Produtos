47673b54246edb7fbcac56b019721612f45665f4
Lucas Schmidt Van Den Eeden Leite <lucas.leite@aguiasistemas.com.br>
2025-05-06 14:12:37 -0300

    Ajuste do banco de dados para notebook Águia

---
+++ Data/Migrations/20250506062702_ImplementaçãoNotebook.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gerenciador_de_Produtos.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementaçãoNotebook : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ItemERPId",
+                table: "Agrupadores");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ItemERPId",
+                table: "Agrupadores",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
